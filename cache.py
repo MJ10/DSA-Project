14377158b72f76a9b5634c19f44b9f690a954c18
Moksh Jain <mokshjn00@gmail.com>
2017-10-30 20:54:43 +0530

    add cache methods

---
+++ cache.py
@@ -0,0 +1,16 @@
+class LFUCache:
+    def __init__(self):
+        """
+        Initializes an object for LFU cache
+        """
+        self.table = HashTable()
+        self.list = LinkedList()
+
+    def add(self):
+        pass
+
+    def evict(self):
+        pass
+
+    def retrieve(self):
+        pass
